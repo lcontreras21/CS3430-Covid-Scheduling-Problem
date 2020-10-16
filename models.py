5fae98dad6c5c8f86ea81d46f5ded9bc8cf44a22
lcontreras21 <luiscontrerasorendain@haverford.edu>
2020-10-16 14:05:52 -0400

    changes to implement graph

--- models.py
+++ models.py
@@ -5,21 +5,23 @@ Changes as of 10/11/2020
 '''
 
 class person:
-	def __init__(self, status):
-		self.classes = []
-		self.status = status 	# to distinguish teachers/students
-		self.neighbors = []
+	def __init__(self, status, s):
+            self.classes = []
+            self.status = status 	# to distinguish teachers/students
+            self.neighbors = []
+            self.number = s
 		
 	def add_class(self, class_no):
 		self.classes += [class_no]
 		
 	def __str__(self):
-		return self.status
+		return self.status + " " + self.number
 		
-class course:
+class Course:
 	def __init__(self, number):
-		self.number = number
-		self.students = []
+            self.number = number
+            self.students = []
+            self.day = ""
 	
 	def add_student(self, s):
 		self.students += [s]
@@ -29,3 +31,32 @@ class course:
 	
 	def __str__(self):
 		return str(self.students)
+
+class graph:
+    # Things to note:
+    #  - to create a new graph, give an empty dictionary along with the dictionary of classes to add
+    #  - graph is a dictionary with person keys and dictionary values, where each person's dictionary
+    # has a course number key which returns a tuple of the days it meets and the people in it
+    def __init__(self):
+        self.graph = {}
+
+    def add_to_graph(self, list_of_classes):
+        for course in list_of_classes.keys():
+            course = list_of_classes[course]
+            course_number = course.number
+            people = course.students
+            day = course.day
+            for person in course.students:  # assume professor in students list
+                if person.number not in self.graph.keys():
+                    self.graph[person.number] = {course_number: (day, people)}
+                else:
+                    self.graph[person.number][course_number] = (day, people)
+    
+    def remove_from_graph(self, courses_to_remove):
+        for person in self.graph:
+            for course in self.graph[person]:
+                if course.number in courses_to_remove:  # to_remove is assumed to be a set of course numbers
+                    del self.graph[person][course.number]
+    
+    def __str__(self):
+        return str(self.graph.keys())
--- preprocessing.py
+++ preprocessing.py
@@ -9,34 +9,28 @@ import csv
 schedule_file = "covid_example_files\demo_schedule_input.txt"	# Can be changed later
 
 def read_in_csv():
-	'''
-	Information is listed as ['Course', 'Room', 'Teacher', 'Time', 'Students']
-	'''
-	#students, teachers = {}, {}
-	classes = {}
-	with open(schedule_file) as csv_file:
-		file_reader = csv.reader(csv_file, delimiter="\t")
-		for row in file_reader:
-			if row[0] not in classes:
-				classes[row[0]] = course(row[0])
-			student_list = row[4].split(" ")
-			for s in student_list:
-				classes[row[0]].add_student(s)
-			'''
-			This chunk of code associates the course with each student
-				Could add all students to the current student's list of neighbors
-				Then at the end call 'set' on list of neighbors to get unique list and remove yourself to get neighbors
-			if row[2] not in teachers:
-				teachers[row[2]] = person("teacher")	
-			teachers[row[2]].add_class(row[0])
-			student_list = row[4].split(" ")
-			for s in student_list:
-				if s not in students:
-					students[s] = person("student")
-				students[s].add_class(row[0])
-			'''
-	return classes
+    '''
+    Information is listed as ['Course', 'Room', 'Teacher', 'Time', 'Students']
+    '''
+    #students, teachers = {}, {}
+    classes = {}
+    our_graph = graph()
+    with open(schedule_file) as csv_file:
+        file_reader = csv.reader(csv_file, delimiter="\t")
+        csv_file.readline()                 # Readline here to skip headers in csv file
+        for row in file_reader:
+                if row[0] not in classes:
+                        classes[row[0]] = Course(row[0])
+                student_list = row[4].split(" ")
+                for s in student_list:
+                        new_student = person("student", s)
+                        classes[row[0]].add_student(new_student)
+                classes[row[0]].day = "Monday"              # Set placeholder day. Need to read in constraints file to figure out day of week
+
+    our_graph.add_to_graph(classes)
+    return our_graph
 			
 			
 if __name__ == "__main__":
-	read_in_csv()
+    final_graph = read_in_csv()
+    print(final_graph.graph["1"])
