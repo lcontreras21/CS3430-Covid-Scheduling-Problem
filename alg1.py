644ed0e371127b430fd5fadbf7e6e1e0406e94d8
Alex Rittler <arittler@haverford.edu>
2020-10-27 10:52:46 -0400

    - couldn't recreate error - added recovered list - fixed calculation of num student seats - fixed incrementation of student infection length - fixed incrementation of time - couldn't figure out why we're getting 19 all the time

--- alg1.py
+++ alg1.py
@@ -4,6 +4,7 @@ import math
 import models
 import alg2
 
+
 def find_in_person(dict_of_classes, contagion_prob, infection_len, semester_len, acceptable_threshold): 
 
     sorted_classes = sorted(dict_of_classes)
@@ -33,12 +34,16 @@ def find_in_person(dict_of_classes, contagion_prob, infection_len, semester_len,
     num_seats_in_person = 0
     for course in sorted_classes[0:size]:
         num_seats_in_person += len(dict_of_classes[course].students)
-    print(f"In person student seats: {num_seats_in_person}")
+    # find the number of teacher 'seats' to remove
+    num_teacher_seats = len(sorted_classes[0:size])
+    # return the number of student seats
+    print(f"In-person Student Seats: {num_seats_in_person - num_teacher_seats}")
     
     num_seats_total = 0
     for course in dict_of_classes:
         num_seats_total += len(dict_of_classes[course].students)
-    print(f"Total possible student seats: {num_seats_total}")
+    num_teacher_seats = len(dict_of_classes)
+    print(f"Best Case Student Seats Value: {num_seats_total - num_teacher_seats}")
 
     return sorted_classes[0:size]
 
