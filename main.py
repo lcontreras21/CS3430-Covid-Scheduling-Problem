8a5515820bee70dfa3213853e423cb86f4414e87
lcontreras21 <luiscontrerasorendain@haverford.edu>
2020-10-28 21:52:17 -0400

    added run.sh and command line arg parsing to main.py file

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import alg1
 import preprocessing
 from models import *
+import sys
+import time
 
 dict_classes = preprocessing.read_in_csv()
 #find_in_person takes dictionary schedule, contagion prob, infect len, sem len, acceptable thresh)
-in_person_final = alg1.find_in_person(dict_classes, 1, 3, 14, 15) 
+if "" in sys.argv or len(sys.argv) != 5:
+    print("Arguments not satisfied.")
+else:
+    in_person_final = alg1.find_in_person(dict_classes, float(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4])) 
 # NOTE: base test input is .1, .3, 7, 1 on demo
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Inputs are Contagion Prob, Infection len, Semester, Len, Acceptable # for Trheshold
+python main.py "$1" "$2" "$3" "$4"
+$SHELL
